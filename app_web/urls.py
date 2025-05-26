cd21e65bf2185ecdd4e5ffe73b1f8803f8e52de5
vq0001 <vquiroga2023@udec.cl>
2025-05-26 12:21:51 -0400

    Subir estructura de urls de app_web

--- app_web/urls.py
+++ app_web/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage),
-    path('seguimiento/<int:codigo_paquete>', views.seguimiento_paquete)
-] 
+    path('login/', views.login),
+    path('seguimiento/<int:codigo_paquete>/', views.seguimiento_paquete),
+    path('usuario<int:id>/', views.usuario),
+    path('usuario<int:id>/estadisticas', views.estadisticas),
+    path('usuario<int:id>/asignacion de rutas', views.asignacion_de_rutas)
+]   
--- app_web/views.py
+++ app_web/views.py
@@ -6,5 +6,17 @@ from django.http import HttpResponse
 def homepage(request):
     return render(request, 'index.html')
 
+def login(request):
+    return render(request, 'login.html')
+
+def usuario(request, id):
+    return HttpResponse('id usuario: %s' % id)
+
+def estadisticas(request): # eficiencia de rutas
+    return HttpResponse('Implementar con BDD')
+
+def asignacion_de_rutas(request):
+    return HttpResponse('Implementar con modulo de rutas')
+
 def seguimiento_paquete(request, codigo_paquete):
     return HttpResponse('codigo paquete: %s' % codigo_paquete)
